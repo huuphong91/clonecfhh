793846632271ff1c1e8ce81e598fbf9fe70ceb8b
caotri25041998 <caotri9725@gmail.com>
2019-03-12 09:11:55 +0700

    asdsad

---
+++ app/src/main/java/com/teamducati/cloneappcfh/entity/Object.java
@@ -0,0 +1,12 @@
+package com.teamducati.cloneappcfh.entity;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Object {
+    @SerializedName("key")
+    private String key1;
+
+    @SerializedName("message")
+    private String tinnhan;
+
+}
