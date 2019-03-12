fdaf067cfd99dedc383b44e7aeb42180a4408500
PhucNguyen32 <phucnh32@fsoft.com.vn>
2019-03-12 16:59:41 +0700

    add. layout news

---
+++ app/src/main/java/com/example/thecoffeehouse/news/NewsAdapter_News.java
@@ -0,0 +1,103 @@
+package com.example.thecoffeehouse.news;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.thecoffeehouse.R;
+
+import java.util.List;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsAdapter_News extends RecyclerView.Adapter<NewsAdapter_News.MyViewHolder> {
+
+    private Context mContext;
+    private List<News> albumList;
+    public boolean vertical=true;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, title_for_news;
+        public ImageView thumbnail, thumbnail_news;
+
+        public MyViewHolder(View view) {
+            super(view);
+//            title = (TextView) view.findViewById(R.id.title_for_you);
+//            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            thumbnail_news = (ImageView) view.findViewById(R.id.thumbnail_for_news);
+            title_for_news = (TextView) view.findViewById(R.id.title_for_news);
+        }
+    }
+
+
+
+    public NewsAdapter_News(Context mContext, List < News > albumList) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder (ViewGroup parent,int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_for_news, parent, false);
+        return new MyViewHolder(itemView);
+
+    }
+
+
+    @Override
+    public void onBindViewHolder ( final MyViewHolder holder, int position) {
+        News album = albumList.get(position);
+//        holder.title.setText(album.getName());
+//        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+        holder.title_for_news.setText((album.getName()));
+        // loading album cover using Glide library
+
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail_news);
+
+
+    }
+
+
+    private void showPopupMenu (View view){
+        PopupMenu popup = new PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_news, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    /**
+     * Click listener for popup menu items
+     */
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_open_news:
+                    Toast.makeText(mContext, "Open News", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getItemCount () {
+        return albumList.size();
+    }
+}
--- app/src/main/java/com/example/thecoffeehouse/news/NewsFragment.java
+++ app/src/main/java/com/example/thecoffeehouse/news/NewsFragment.java
@@ -1,6 +1,9 @@
 package com.example.thecoffeehouse.news;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +12,23 @@ import android.widget.TextView;
 
 import com.example.thecoffeehouse.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class NewsFragment extends Fragment {
-
+    View rootView;
+    private NewsAdapter adapter;
+    private NewsAdapter_News adapter2;
+    private List<News> albumList, albumListVer;
     private TextView mTxtName, mTxtLogin;
     private ImageView mImgAccount, mImgNotification;
 
@@ -27,7 +41,68 @@ public class NewsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        rootView = inflater.inflate(R.layout.fragment_news, container, false);
+        final RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.for_you_recycleview);
+        final RecyclerView recyclerViewNews = (RecyclerView) rootView.findViewById(R.id.news_recycleview);
+        albumList = new ArrayList<>();
+        albumListVer = new ArrayList<>();
+        adapter = new NewsAdapter(getContext(), albumList);
+        adapter2 = new NewsAdapter_News(getContext(), albumListVer);
+
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, true);
+        recyclerView.setLayoutManager(mLayoutManager);
+        //recyclerView.addItemDecoration(new GridSpacingItemDecoration(8, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        recyclerViewNews.setVerticalFadingEdgeEnabled(true);
+        recyclerViewNews.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewNews.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewNews.setAdapter(adapter2);
+        prepareAlbums();
+        return rootView;
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
     @Override
@@ -40,6 +115,42 @@ public class NewsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
+    }
+
+    private void prepareAlbums() {
+        int[] covers = new int[]{
+                R.drawable.images_coffe,
+                R.drawable.images_coffe2,
+                R.drawable.images_coffe3,
+                R.drawable.images_coffe4,
+                R.drawable.images_coffe5,
+                R.drawable.images_coffe6,
+                R.drawable.images_coffe7,
+                R.drawable.images_coffe8,
+        };
+
+        News a = new News("Coffee One Dotta", covers[0]);
+        albumList.add(a);
+        a = new News("Tra Dao 2 ", covers[1]);
+        albumList.add(a);
+        albumListVer.add(a);
+        a = new News("Tra Dao 5", covers[2]);
+        albumList.add(a);
+
+        a = new News("Coffee với sữa", covers[3]);
+        albumList.add(a);
+        a = new News("Coffe Cabuchino", covers[4]);
+        albumList.add(a);
+        a = new News("Coffe House Sữa", covers[5]);
+        albumList.add(a);
+        a=new News("Coffee được làm để uống kèm với bánh co tác dung làm cho cơ thể đủ sức tỉnh táo...",covers[6]);
+        albumListVer.add(a);
+        a=new News("Coffee được làm để tăng sức khỏe kèm khi bạn uống ít tác dung làm cho cơ thể đủ sức tỉnh táo...",covers[7]);
+        albumListVer.add(a);
+//        a=new News("Coffee được làm để tăng sức khỏe kèm khi bạn uống cùng bạn bè nhân dịp lễ 8/3 của thế giới phụ nữ kỉ niệm điều tuyệt vời...",covers[8]);
+//        albumListVer.add(a);
+        adapter.notifyDataSetChanged();
+        adapter2.notifyDataSetChanged();
     }
 
     private void initView(View view) {
