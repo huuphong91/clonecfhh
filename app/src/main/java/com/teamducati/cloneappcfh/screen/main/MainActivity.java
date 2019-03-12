3f6180586b5b78c2475d7808be06fd5207db54b2
huupt <huupt@fsoft.com.vn>
2019-03-12 16:50:49 +0700

    imp.ship_address

--- app/src/main/java/com/teamducati/cloneappcfh/screen/main/MainActivity.java
+++ app/src/main/java/com/teamducati/cloneappcfh/screen/main/MainActivity.java
@@ -1,9 +1,22 @@
 package com.teamducati.cloneappcfh.screen.main;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.teamducati.cloneappcfh.R;
@@ -17,6 +30,13 @@ import com.teamducati.cloneappcfh.screen.order.OrderPresenter;
 import com.teamducati.cloneappcfh.screen.store.StoreFragment;
 import com.teamducati.cloneappcfh.screen.store.StorePresenter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import androidx.core.app.ActivityCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -26,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView mNavigationView;
     @BindView(R.id.viewPager)
     MainViewPager mViewPager;
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private FusedLocationProviderClient mFusedLocation;
+    private Location mCurrentLocation;
 
     private NewsPresenter mNewsPresenter;
     private OrderPresenter mOrderPresenter;
@@ -44,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(Objects.requireNonNull(this));
+
         initUI();
     }
 
@@ -98,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.navigation_order:
                 positionFragment = 1;
                 setCurrentItem(positionFragment);
+                getLocation();
                 return true;
             case R.id.navigation_store:
                 positionFragment = 2;
@@ -113,5 +141,95 @@ public class MainActivity extends AppCompatActivity {
 
     private void setCurrentItem(int position) {
         mViewPager.setCurrentItem(position);
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            mFusedLocation.getLastLocation().addOnSuccessListener(location -> {
+                if (location != null) {
+                    new FetchAddressTask().execute(location);
+                }
+            });
+        }
+        mOrderFragment.setLocation("Loading...");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Toast.makeText(this,
+                            "Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private class FetchAddressTask extends AsyncTask<Location, Void, String> {
+
+        @Override
+        protected String doInBackground(Location... locations) {
+            Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+            Location location = locations[0];
+            List<Address> addresses = null;
+            String resultMessage = "";
+            try {
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        // In this sample, get just a single address
+                        1);
+                if (addresses == null || addresses.size() == 0) {
+                    if (resultMessage.isEmpty()) {
+                        resultMessage = "No address Found";
+                        Log.e("TAG", resultMessage);
+                    }
+                } else {
+                    // If an address is found, read it into resultMessage
+                    Address address = Objects.requireNonNull(addresses).get(0);
+                    ArrayList<String> addressParts = new ArrayList<>();
+
+                    // Fetch the address lines using getAddressLine,
+                    // join them, and send them to the thread
+                    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        addressParts.add(address.getAddressLine(i));
+                    }
+                    Log.d("addressParts", addressParts.get(0));
+
+                    resultMessage = address.getSubThoroughfare()+ address.getThoroughfare();
+                    Log.d("resultMessageFace", resultMessage);
+                }
+            } catch (IOException ioException) {
+                // Catch network or other I/O problems
+                resultMessage = "Not available";
+                Log.e("TAG", resultMessage, ioException);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                // Catch invalid latitude or longitude values
+                resultMessage = "invalid_lat_long_used";
+                Log.e("TAG", resultMessage + ". " +
+                        "Latitude = " + location.getLatitude() +
+                        ", Longitude = " +
+                        location.getLongitude(), illegalArgumentException);
+            }
+
+            return resultMessage;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            mOrderFragment.setLocation(s);
+            super.onPostExecute(s);
+        }
     }
 }
--- app/src/main/java/com/teamducati/cloneappcfh/screen/order/OrderFragment.java
+++ app/src/main/java/com/teamducati/cloneappcfh/screen/order/OrderFragment.java
@@ -1,23 +1,50 @@
 package com.teamducati.cloneappcfh.screen.order;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.teamducati.cloneappcfh.R;
+import com.teamducati.cloneappcfh.screen.main.MainActivity;
 import com.teamducati.cloneappcfh.screen.news.NewsContract;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OrderFragment extends Fragment implements OrderContract.View {
 
+    @BindView(R.id.toolBarShipLocation)
+    ConstraintLayout mToolBarShipLocation;
+    @BindView(R.id.tvShipAddress)
+    TextView tvShipAddress;
+
     private OrderContract.Presenter mPresenter;
+
+    private Unbinder unbinder;
 
     public OrderFragment() {
         // Required empty public constructor
@@ -27,13 +54,23 @@ public class OrderFragment extends Fragment implements OrderContract.View {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View view = inflater.inflate(R.layout.fragment_order, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
-
 
     @Override
     public void setPresenter(OrderContract.Presenter presenter) {
         mPresenter = presenter;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    public void setLocation(String location) {
+        tvShipAddress.setText(location);
     }
 }
