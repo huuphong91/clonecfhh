14c413c2c268d4b9ba04fd3e7009ef0ef9e026fd
huupt <huupt@fsoft.com.vn>
2019-03-12 10:40:50 +0700

    fix.first_merge_request

--- app/src/main/java/com/teamducati/cloneappcfh/screen/news/NewsPresenter.java
+++ app/src/main/java/com/teamducati/cloneappcfh/screen/news/NewsPresenter.java
@@ -85,14 +85,6 @@ public class NewsPresenter implements NewsContract.Presenter {
                 });
     }
 
-
-    private NewsContract.View mNewsView;
-
-    public NewsPresenter(NewsContract.View newsView) {
-        this.mNewsView = newsView;
-        mNewsView.setPresenter(this);
-    }
-
     @Override
     public void start() {
 
