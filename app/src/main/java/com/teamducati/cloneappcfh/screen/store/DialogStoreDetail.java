16d47a38b547c8ee6184f711bfb62191b0a710ec
LuanNTH <luan20496@gmail.com>
2019-03-21 16:34:41 +0700

    mod detail store

--- app/src/main/java/com/teamducati/cloneappcfh/screen/store/DialogStoreDetail.java
+++ app/src/main/java/com/teamducati/cloneappcfh/screen/store/DialogStoreDetail.java
@@ -4,8 +4,11 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,11 +19,12 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.teamducati.cloneappcfh.R;
+import java.util.List;
 import androidx.appcompat.widget.Toolbar;
 
 public class DialogStoreDetail {
 
-    public void showDialog(Context context, String name, String image, Double lat, Double lon, String address,
+    public void showDialog(Context context, String name, List<String> listImage, Double lat, Double lon, String address,
                            String time, String numberPhone) {
         Dialog dialog = new Dialog(context, android.R.style.Theme_Light);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -34,14 +38,26 @@ public class DialogStoreDetail {
                 dialog.dismiss();
             }
         });
+
+        ViewFlipper mStroreDetail = dialog.findViewById(R.id.imgStroreDetail);
+        for (int i = 0; i < listImage.size(); i++) {
+            ImageView imageView = new ImageView(context);
+            Glide.with(context)
+                    .load(listImage.get(i))
+                    .placeholder(R.drawable.common_full_open_on_phone)
+                    .into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            mStroreDetail.addView(imageView);
+        }
+        mStroreDetail.setFlipInterval(5000);
+        mStroreDetail.setAutoStart(true);
+        Animation animation_in_right = AnimationUtils.loadAnimation(context, R.anim.slide_in_right);
+        Animation animation_out_right = AnimationUtils.loadAnimation(context, R.anim.slide_out_left);
+        mStroreDetail.setInAnimation(animation_in_right);
+        mStroreDetail.setOutAnimation(animation_out_right);
+
         TextView mNameStoreMap = dialog.findViewById(R.id.txtNameStroreDetail);
         mNameStoreMap.setText(name);
-
-        ImageView mImgStroreDetail = dialog.findViewById(R.id.imgStroreDetail);
-        Glide.with(context)
-                .load(image)
-                .placeholder(R.drawable.common_full_open_on_phone)
-                .into(mImgStroreDetail);
 
         MapView mMapView = (MapView) dialog.findViewById(R.id.mpvStroreDetail);
         MapsInitializer.initialize(context);
