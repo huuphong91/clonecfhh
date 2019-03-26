00fb40d5e9f4c42494f0f402c3a0158e70fa4ba9
PhuLVT <phulvt@fsoft.com.vn>
2019-03-26 10:18:34 +0700

    fix. clean code

--- app/src/main/java/com/teamducati/cloneappcfh/screen/splash/SplashScreenFragment.java
+++ app/src/main/java/com/teamducati/cloneappcfh/screen/splash/SplashScreenFragment.java
@@ -1,77 +1,55 @@
 package com.teamducati.cloneappcfh.screen.splash;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.teamducati.cloneappcfh.R;
-import com.teamducati.cloneappcfh.screen.main.MainActivity;
 
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import io.reactivex.Single;
+
+@SuppressWarnings("ALL")
 public class SplashScreenFragment extends DialogFragment {
-    private WebView webViewNews;
-    private ImageView imgBackWebView;
-    private TextView txtTitleWebView;
-    private String mUrl = "";
-    private String mTitle = "";
-    private View view;
-    private Bundle bundle;
-    private Dialog dialog;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_sqlash_srcreen, container, false);
-        initData();
-        initMappingViewId();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sqlash_srcreen, container, false);
         initEvent();
         return view;
     }
 
+    @SuppressLint("CheckResult")
     private void initEvent() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                dismiss();
-            }
-        }, 3000);
+        Single.just(1).delay(3000, TimeUnit.MILLISECONDS).subscribe(integer ->
+                Objects.requireNonNull(getDialog()).dismiss());
     }
-
-
+    
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        dialog = super.onCreateDialog(savedInstanceState);
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        Objects.requireNonNull(dialog.getWindow()).requestFeature(Window.FEATURE_NO_TITLE);
         return dialog;
     }
-
-    private void initData() {
-
-    }
-
-    private void initMappingViewId() {
-
-    }
-
 
     @Override
     public void onStart() {
         super.onStart();
         Dialog dialog = getDialog();
         if (dialog != null) {
-            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            Objects.requireNonNull(dialog.getWindow()).setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         }
     }
